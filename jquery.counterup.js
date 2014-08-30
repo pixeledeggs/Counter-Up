2aca25ba67f63c46ef141b7d4dbcb83be012c66b
Arun Stephens <arun@pixeledeggs.com>
2014-08-30 22:13:00 +0100

    added callback

--- jquery.counterup.js
+++ jquery.counterup.js
@@ -14,7 +14,8 @@
     // Defaults
     var settings = $.extend({
         'time': 400,
-        'delay': 10
+        'delay': 10,
+        'callback': null
     }, options);
 
     return this.each(function(){
@@ -66,6 +67,10 @@
                     delete $this.data('counterup-nums');
                     $this.data('counterup-nums', null);
                     $this.data('counterup-func', null);
+
+                    if (typeof($settings.callback) === 'function') {
+                        $settings.callback();
+                    }
                 }
             };
             $this.data('counterup-func', f);
